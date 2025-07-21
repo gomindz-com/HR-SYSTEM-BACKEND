98e70427df7b2b6ae0b176f15c1b744de748b08f
Sannabs <sannabsjammeh54@gmail.com>
2025-07-21 15:02:03 +0000

    Integrate token verification middleware into the send-invitation route for enhanced security

--- routes/invitation.route.js
+++ routes/invitation.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { acceptInvitation, sendInvitation } from "../controller/invitation.controller.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/send-invitation", sendInvitation);
+router.post("/send-invitation", verifyToken, sendInvitation);
 router.post("/accept-invitation/:token", acceptInvitation);
 
 export default router;
