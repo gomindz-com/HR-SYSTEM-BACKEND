e663a9c229bbb976dacda26e2dc727474c4b95f2
Sannabs <sannabsjammeh54@gmail.com>
2025-08-01 13:08:44 +0000

    Add getEmployeeDetails endpoint to employee controller for fetching specific employee information, including department and attendance details, and update routes to include this new endpoint.

--- routes/employee.route.js
+++ routes/employee.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.middleware.js";
-import { listEmployees } from "../controller/employee.controller.js";
+import {
+  listEmployees,
+  getEmployeeDetails,
+} from "../controller/employee.controller.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, listEmployees);
+router.get("/:id", verifyToken, getEmployeeDetails);
 
 export default router;
